7b5d8b478e13a3dfa25248d381cf30ea556f04da
nm-dl1212 <106422456+nm-dl1212@users.noreply.github.com>
2024-10-20 15:15:23 +0000

    mod: save notebook

--- main_mlflow.py
+++ main_mlflow.py
@@ -1,12 +1,20 @@
-import os
-import sqlite3
+from datetime import datetime
 import papermill as pm
-import mlflow
 
+EXPERIMENT_NAME = f"exp_{datetime.now():%Y%m%d%H%M%S}"
+NOTEBOOK_NAME = "main.ipynb"
+OUTPUT_NOTEBOOK_NAME = "main_out.ipynb"
 
 for i, c in enumerate(["blue", "red", "green"]):
     pm.execute_notebook(
         'main.ipynb',
         'main_out.ipynb',
-        parameters=dict(COLOR=c, EXPERIMENT_NAME=f'exp_{i:02}')
+        parameters=dict(
+            EXPERIMENT_NAME=EXPERIMENT_NAME,
+            NOTEBOOK_NAME=OUTPUT_NOTEBOOK_NAME,
+            PARAMETERS={ 
+                "datasetPath": "data/sample.csv",
+                "color": c
+            }
+        )
     )
